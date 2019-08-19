6973899aa66b0a6662d6c2779f916c85c988d314
Andrew Puglionesi <aop4@pitt.edu>
2019-08-18 23:42:13 -0400

    Tweak wording

--- src/views/home/Home.js
+++ src/views/home/Home.js
@@ -31,7 +31,7 @@ export default class Home extends Component {
                                 ¡Hola! Este sitio se propone reflejar todos que hago—y nunca me contento con hacer solo una cosa. Soy un un ingeniero de software, pero también soy un escritor, músico, y ecologista.
                             </Then>
                             <Else>
-                                Hi there! This website is meant to reflect everything I do—and I was never content to do just one thing. I am a software engineer, but I am also a writer, musician, and environmental advocate based in Pittsburgh.
+                                Hi there! This website is meant to reflect everything I do—and I was never content to do just one thing. I am a software engineer, but I am also a writer, musician, and environmental advocate.
                             </Else>
                         </If>
                     </p>
@@ -41,7 +41,7 @@ export default class Home extends Component {
                                 En cuanto a mi carrera de ingeniería, me considero un generalista: he trabajado en el front-end y el back-end de sistemas diferentes, y tengo experiencia con una variedad de lenguas y entornos de trabajo. Me apasiona aprender cosas nuevas, y estoy un poco obsesionado con escribir código legible.
                             </Then>
                             <Else>
-                                Regarding my engineering career, I consider myself a generalist, with experience at both ends in a variety of languages and frameworks. I am passionate about learning new things and kind of obsessed with writing readable code.
+                                Regarding my engineering work, I consider myself a generalist, with experience at both ends in a variety of languages and frameworks. I am passionate about learning new things and kind of obsessed with writing readable code.
                             </Else>
                         </If>
                     </p>
@@ -51,7 +51,7 @@ export default class Home extends Component {
                                 Este sitio es un documento vivo. Existe para mostrar todo mi trabajo en un lugar, organizar ese trabajo, y desarrollar junto conmigo.
                             </Then>
                             <Else>
-                                This site is very much a living document. It's here to tie all my work together, organize it, and develop alongside me. Go explore.
+                                This site itself is very much a living document. It's here to tie all my work together, organize it, and develop alongside me. Go ahead and explore...
                             </Else>
                         </If>
                     </p>
